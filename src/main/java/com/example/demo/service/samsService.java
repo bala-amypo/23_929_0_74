ae8f3f70aa823b847c2d132d1ba607d84730a1e4
bala-amypo <bala@amypo.in>
2025-12-15 13:23:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/samsService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.sams;
+
+@Service
+public class samsService {
+    
+public sams saveDatas(sams sms){
+
+    
+}
+
+}
