374ddedc9ee300555f9dbbafc4d1cd9421867c69
bala-amypo <bala@amypo.in>
2025-12-15 17:52:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,14 +1,14 @@
 package com.example.demo.entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Location {
-    @Id
-    @Generated(strategy=GenerationType.IDENTITY)
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
 private Long id;
 private String name;
 private Double latitude;
