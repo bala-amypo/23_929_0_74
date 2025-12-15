cb901360b2d9c05707dd2c07a09b5d90dbeada06
bala-amypo <bala@amypo.in>
2025-12-15 16:28:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,15 +1,49 @@
 package com.example.demo.entity;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Location {
-    
+    @Id
+    @Generated(stater)
 private Long id;
 private String name;
 private Double latitude;
-private Double latitude;
-    
+private Double longtude;
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public Double getLatitude() {
+    return latitude;
+}
+public void setLatitude(Double latitude) {
+    this.latitude = latitude;
+}
+public Double getLongtude() {
+    return longtude;
+}
+public void setLongtude(Double longtude) {
+    this.longtude = longtude;
+}
+public Location(Long id, String name, Double latitude, Double longtude) {
+    this.id = id;
+    this.name = name;
+    this.latitude = latitude;
+    this.longtude = longtude;
+}
+public Location() {
+}    
 
 
 
