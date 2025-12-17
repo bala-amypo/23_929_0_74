d8604f88750fd2441354bada482ba8c9bfeb4ecd
bala-amypo <bala@amypo.in>
2025-12-17 03:42:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -28,6 +28,6 @@ return lcs.createLocation(loc);
 public List<Location> getAll(){
     return lcs.getAllLocations();
 }
-public Optional<Location> get(@PathVar)
+public Optional<Location> get()
 
 }
