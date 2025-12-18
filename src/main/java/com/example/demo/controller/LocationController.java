c19e900a1c0fa7ef5963e0acde644bfc5d2a619b
bala-amypo <bala@amypo.in>
2025-12-18 08:47:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -17,32 +17,37 @@ import com.example.demo.service.LocationService;
 import org.springframework.http.ResponseEntity;
 
 @RestController
+
 public class LocationController {
     
 @Autowired
 LocationService  lcs;
 
+// for post the data
 @PostMapping("/location")
 public Location addLocation(@RequestBody Location loc){
 
 return lcs.createLocation(loc);
 }
 
+// for get all the data in list view
 @GetMapping
 public List<Location> getAll(){
     return lcs.getAllLocations();
 }
 
+
 @GetMapping("/{id}")
 public Location getStudent(@PathVariable Long id) {
 return lcs.getViewByID(id);
 }
-// UPDATE
+
+
 @PutMapping("/{id}")
 public Location updateStudent(@PathVariable Long id,@Valid @RequestBody Location student){
 return lcs.updateStudent(id, student);
 }
-// DELETE
+
 @DeleteMapping("/{id}")
 public ResponseEntity<String> deleteStudent(@PathVariable Long id) {
 lcs.deleteStudent(id);
